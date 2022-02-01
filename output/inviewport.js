f80cf775f7ad1cf744ed86aa5fde38a47b9199dd
Kamil <kgraf@interia.pl>
2022-02-01 02:55:23 +0000

    tsconfit.json: changed "module": "umd" > "commonjs" to eliminate the error "Critical dependency: require function is used in a way in which dependencies cannot be statically extracted"

--- output/inviewport.js
+++ output/inviewport.js
@@ -1,54 +1,44 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    exports.__esModule = true;
-    function inViewport(element, threshold, callback, advancedConfiguration) {
-        if (advancedConfiguration === void 0) { advancedConfiguration = null; }
-        if ('IntersectionObserver' in window && element) {
-            var safeRoot = advancedConfiguration && advancedConfiguration.root
-                ? advancedConfiguration.root
-                : null;
-            var safeRootMargin = advancedConfiguration && advancedConfiguration.rootMargin
-                ? advancedConfiguration.rootMargin
-                : '0px';
-            var config = {
-                root: safeRoot,
-                rootMargin: safeRootMargin,
-                threshold: (Array.isArray(threshold) && threshold.length > 0) ||
-                    (threshold >= 0 && threshold <= 1)
-                    ? threshold
-                    : 0.5
-            };
-            var observer = new IntersectionObserver(function (entries, observer) {
-                entries.forEach(function (change) {
-                    if (change.isIntersecting === true && change.intersectionRatio > 0) {
-                        if (Array.isArray(callback)) {
-                            callback[0]();
-                            if (callback.length === 1) {
-                                observer.unobserve(change.target);
-                            }
-                        }
-                        else {
-                            callback();
+"use strict";
+exports.__esModule = true;
+function inViewport(element, threshold, callback, advancedConfiguration) {
+    if (advancedConfiguration === void 0) { advancedConfiguration = null; }
+    if ('IntersectionObserver' in window && element) {
+        var safeRoot = advancedConfiguration && advancedConfiguration.root
+            ? advancedConfiguration.root
+            : null;
+        var safeRootMargin = advancedConfiguration && advancedConfiguration.rootMargin
+            ? advancedConfiguration.rootMargin
+            : '0px';
+        var config = {
+            root: safeRoot,
+            rootMargin: safeRootMargin,
+            threshold: (Array.isArray(threshold) && threshold.length > 0) ||
+                (threshold >= 0 && threshold <= 1)
+                ? threshold
+                : 0.5
+        };
+        var observer = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (change) {
+                if (change.isIntersecting === true && change.intersectionRatio > 0) {
+                    if (Array.isArray(callback)) {
+                        callback[0]();
+                        if (callback.length === 1) {
+                            observer.unobserve(change.target);
                         }
                     }
-                    if (change.isIntersecting === false) {
-                        if (Array.isArray(callback) && typeof callback[1] === 'function') {
-                            callback[1]();
-                        }
+                    else {
+                        callback();
                     }
-                });
-            }, config);
-            observer.observe(element);
-        }
+                }
+                if (change.isIntersecting === false) {
+                    if (Array.isArray(callback) && typeof callback[1] === 'function') {
+                        callback[1]();
+                    }
+                }
+            });
+        }, config);
+        observer.observe(element);
     }
-    exports["default"] = inViewport;
-});
+}
+exports["default"] = inViewport;
 //# sourceMappingURL=inviewport.js.map
